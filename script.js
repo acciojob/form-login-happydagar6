4f8aa909e1e3a257428f055c0c4a22c8419299fd
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-13 17:49:01 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-function getFormvalue() {
-    //Write your code here
-	event.preventDefault();
-	const form = document.getElementById("form1");
-	const fistName = form.elements["fname"].value.trim();
-	const lastName = form.elements["lname"].value.trim();
-	const fullName = firstName + " " + lastName;
-	alert(fullName);
+function getFormvalue(event) {
+    event.preventDefault();
+
+    const form = document.getElementById("form1");
+
+    const firstName = form.elements["fname"].value.trim();
+    const lastName = form.elements["lname"].value.trim();
+
+    const fullName = (firstName + " " + lastName).trim();
+
+    alert(fullName);
 }
