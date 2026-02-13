15da79ae462fea2edea538b6ef1f5741aae4f51d
Kapil Dagar <116935052+happydagar6@users.noreply.github.com>
2026-02-13 17:46:38 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
 function getFormvalue() {
     //Write your code here
-
+	event.preventDefault();
+	const form = document.getElementById("form1");
+	const fistName = form.elements["fname"].value.trim();
+	const lastName = form.elements["lname"].value.trim();
+	const fullName = firstName + " " + lastName;
+	alert(fullName);
 }
